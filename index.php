fd02066a02d53943799694eaf5ed022c26ccf2f8
Junnyper <kim_23_93@hotmail.com>
2019-09-23 17:37:43 +0200

    Versión inicial

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
-	if(isset($_GET['email']))
+	if(isset($_GET['email'])){
 		print_r($_GET);
 
 #guardar suscriptores
 	$f=fopen('suscriptores.txt','a');
 	fwrite($f,$_GET['email']."\r\n");
 fclose($f)
+}
 ?>
 
 <form>
