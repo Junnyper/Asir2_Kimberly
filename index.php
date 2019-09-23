ac0a2d85b730382c9ed0a9966b1726749acaf365
Junnyper <kim_23_93@hotmail.com>
2019-09-23 17:21:16 +0200

    Versión inicial

--- index.php
+++ index.php
@@ -3,11 +3,15 @@
 	if(isset($_GET))
 		print_r($_GET);
 
+#guardar suscriptores
+	$f=fopen('suscriptores.txt','a');
+	fwrite($f,$_GET['email']."\r\n");
+fclose($f)
 ?>
 
 <form>
 
 	<input name="email"/>
-	<button> Suscribe </button>
+	<button> Subscribe </button>
 	
 </form>
