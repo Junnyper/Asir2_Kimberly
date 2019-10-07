f8301e898462490f88dd9e4ed0277ee772d0433c
Junnyper <kim_23_93@hotmail.com>
2019-10-07 16:09:56 +0200

    Variables

--- 2a.php
+++ 2a.php
@@ -1,5 +1,23 @@
 <?php
-$A=2
-$B=654635214
-.$A+$B;
+$a=2;
+$b=8;
+echo '<div>';
+echo 'Suma de '.$a.' + '.$b.' = ';
+echo $a+$b;
+echo '</div>';
+
+echo '<div>';
+echo 'Resta de '.$a.' - '.$b.' = ';
+echo $a-$b;
+echo '</div>';
+
+echo '<div>';
+echo 'Multiplicación de '.$a.' x '.$b.' = ';
+echo $a*$b;
+echo '</div>';
+
+echo '<div>';
+echo 'División de '.$a.' / '.$b.' = ';
+echo $a/$b;
+echo '</div>'; 
 ?>
