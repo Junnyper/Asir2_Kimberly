7a20876091c3628d136deced2b333a6bda599945
Junnyper <kim_23_93@hotmail.com>
2019-11-08 20:04:56 +0100

    Ay

---
+++ html.php
@@ -0,0 +1,42 @@
+ <!DOCTYPE html>
+ 
+	<html style="background-color:#ffcc99;">
+		<head>
+			<meta charset="UTF-8">
+			<title>Título de la web</title>
+		</head>
+	<body>
+			<h1 style="background-color:DodgerBlue;">Título utilizando h1</h1>
+			
+				<p>En <u>este párrafo</u> <del>la letra cambia</del> pasan cosas.</p>
+			
+			<h2> <color="MediumSeaGreen;"> Párrafo utilizando h2</color></h2>
+			
+				<p>Este <b>párrafo</b> tiene algo <mark>raro</mark></p>
+				
+			<h3 style="border:2px solid Violet;">En el último utilizamos h3</h3>
+			
+				 <img src="gato.jpg" alt="Gatito" style="width:300px;height:300px;"> 
+				<form action="/action_page.php" method="get">
+						Nombre: <input type="text" name="fname"><br>
+					<input type="submit" value="Submit">
+				</form> 
+			 <table style="width:50%" border="1px">
+				<tr>
+					<th>Primera columna</th>
+					<th>Segunda columna</th>
+					<th>Numeritos</th>
+				</tr>
+				<tr>
+					<td>Hola</td>
+					<td>Caracola</td>
+					<td>18</td>
+				</tr>
+				<tr>
+					<td>Adiós</td>
+					<td>Caracól</td>
+					<td>6</td>
+				</tr>
+			</table> 
+	</body>
+	</html> 
