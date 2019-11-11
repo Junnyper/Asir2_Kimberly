8137ba506deb0afe7c5a78997fc207a23f6f6d67
Junnyper <kim_23_93@hotmail.com>
2019-11-11 15:50:07 +0100

    Ay

--- Triangulos_c.php
+++ Triangulos_c.php
@@ -1,75 +1,73 @@
-<form>
-	Ángulo 1    
-	<input name="angulo1"/>
-	</br>
-	Ángulo 2    
-	<input name="angulo2"/>
-	</br>
-	Ángulo 3  
-	<input name="angulo3"/>
-	</br>
-	<button>Comprobar</button>
-	</br>
-	</br>
-</form>
+	<form>
+			Número 1 -
+		<input name="angulo1"/> </br>
+			Número 2 -
+		<input name="angulo2"/> </br>
+			Número 3 -
+		<input name="angulo3"/> </br></br>
+	<button>Comprobación</button> </br></br>
+	</form>
 
-<form>
-	<button href="http://localhost/asir2_alvaro/triangulos_c.php">Poner a 0 los valores.</button>
+	<form>
+		<button href="http://localhost/asir2_kimberly/triangulos_c.php">Poner a 0 los valores.</button>
 	</br>
-	</br>
-</form>
+	</form>
 
 <?php
-function triangulo($a,$b,$c,$A,$B,$C){
+
+	function triangulo($a,$b,$c,$A,$B,$C){
 	
-	$a=$A;
-	$b=$B;
-	$c=$C;
+		$a=$A;
+		$b=$B;
+		$c=$C;
 	
-	echo lados($a,$b,$c);
-	echo angulos($A,$B,$C);
+			echo lados($a,$b,$c);
+			echo angulos($A,$B,$C);
 	
-}
-function lados($a,$b,$c){
+	}
+	function lados($a,$b,$c){
 	
-	if(is_numeric($a) && is_numeric($b) && is_numeric($c) && ($a)>0 && ($b)>0 && ($c)>0){
+		if(is_numeric($a) && is_numeric($b) && is_numeric($c) && ($a)>0 && ($b)>0 && ($c)>0){
 			if($a==$b && $a==$c){
-				$r='Triángulo equilátero ';
+				$r='Equilátero ';
 			}elseif($a==$b || $a==$c || $b==$c){
-				$r='Triángulo isósceles ';
+				$r='Isósceles ';
 			}else
-				$r='Triángulo escaleno ';
+				$r='Escaleno ';
 			return $r;
 				
 	}
 }
-function angulos($A,$B,$C){
-	if(is_numeric($A) && is_numeric($B) && is_numeric($C) && ($A)>0 && ($B)>0 && ($C)){
-		if((($A)+($B)+($C))==180){
-			if($A==90 || $B==90 || $C==90){
-				$r='y rectángulo.';
-			}elseif($A>90 || $B>90 || $C>90){
-				$r='y obtusángulo.';
-			}else
-				$r='y acutángulo.';
-			return $r;
+	function angulos($A,$B,$C){
+	
+		if(is_numeric($A) && is_numeric($B) && is_numeric($C) && ($A)>0 && ($B)>0 && ($C)){
+			if((($A)+($B)+($C))==180){
+				if($A==90 || $B==90 || $C==90){
+					$r='y rectángulo.';
+				}elseif($A>90 || $B>90 || $C>90){
+					$r='y obtusángulo.';
+				}else
+					$r='y acutángulo.';
+				return $r;
 			
-		}else
+			}else
+		
 			echo 'Los ángulos de un triángulo han de sumar 180º, revisa que has metido.';
 	}else
-		echo 'Introduce valores numéricos, por favor.';
+			
+			echo 'Introduce valores numéricos, por favor.';
 	
 }
-if(isset($_GET['angulo1'], $_GET['angulo2'], $_GET['angulo3'])){
+		if(isset($_GET['angulo1'], $_GET['angulo2'], $_GET['angulo3'])){
 	
-	echo triangulo(1,1,1,($_GET['angulo1']),($_GET['angulo2']),($_GET['angulo3']));
+			echo triangulo(1,1,1,($_GET['angulo1']),($_GET['angulo2']),($_GET['angulo3']));
 	
 }else
-	echo 'Introduce la medida de los angulos del triángulo.';
-echo '<br><br>Vectores de test:<br><br>';
-echo triangulo(1,sqrt(2),1,45,90,45).' ¿rectángulo-isósceles?<br/>';
-echo triangulo(1,sqrt(2),1,45,45,90).' ¿rectángulo-isósceles?<br/>';
-echo triangulo(1,sqrt(2),1,90,45,45).' ¿rectángulo-isósceles?<br/>';
-echo triangulo(1,1,1,60,60,60).' ¿acutángulo-equilátero?<br/>';
-echo triangulo(1,1,1,100,40,40).' ¿obtusángulo-isósceles?<br/>';
+			echo 'Introduce las medidas del triángulo.';
+			echo '<br><br>Test:<br><br>';
+			echo triangulo(1,sqrt(2),1,45,90,45).' rectángulo-isósceles<br/>';
+			echo triangulo(1,sqrt(2),1,45,45,90).' rectángulo-isósceles<br/>';
+			echo triangulo(1,sqrt(2),1,90,45,45).' rectángulo-isósceles<br/>';
+			echo triangulo(1,1,1,60,60,60).' acutángulo-equilátero<br/>';
+			echo triangulo(1,1,1,100,40,40).' obtusángulo-isósceles<br/>';
 ?>
--- html.php
+++ html.php
@@ -5,16 +5,16 @@
 			<meta charset="UTF-8">
 			<title>Título de la web</title>
 		</head>
-	<body>
-			<h1 style="background-color:DodgerBlue;">Título utilizando h1</h1>
+	<body><center>
+			<h1> <font color="dodgerblue">Título utilizando h1</font></h1>
 			
-				<p>En <u>este párrafo</u> <del>la letra cambia</del> pasan cosas.</p>
+				<p><font size="28px">En <u>este párrafo</u> <del>la letra cambia</del> pasan cosas.</font></p>
 			
-			<h2> <color="MediumSeaGreen;"> Párrafo utilizando h2</color></h2>
+			<h2> <font color="MediumSeaGreen">Párrafo utilizando h2</font></h2>
 			
-				<p>Este <b>párrafo</b> tiene algo <mark>raro</mark></p>
+				<p>Este <b>párrafo</b> tiene algo <mark>raro.</mark></p>
 				
-			<h3 style="border:2px solid Violet;">En el último utilizamos h3</h3>
+			<h3>En el último utilizamos h3.</h3>
 			
 				 <img src="gato.jpg" alt="Gatito" style="width:300px;height:300px;"> 
 				<form action="/action_page.php" method="get">
@@ -23,20 +23,26 @@
 				</form> 
 			 <table style="width:50%" border="1px">
 				<tr>
-					<th>Primera columna</th>
-					<th>Segunda columna</th>
-					<th>Numeritos</th>
+					<th>Provincia</th>
+					<th>Costa (Km)</th>
 				</tr>
 				<tr>
-					<td>Hola</td>
-					<td>Caracola</td>
-					<td>18</td>
+					<td>Galicia</td>
+					<td>1676</td>
 				</tr>
 				<tr>
-					<td>Adiós</td>
-					<td>Caracól</td>
-					<td>6</td>
+					<td>Principado de Asturias</td>
+					<td>534</td>
+				</tr>
+				<tr>
+					<td>País Vasco</td>
+					<td>379</td>
+				</tr>
+				<tr>
+					<td>Cantábria</td>
+					<td>284</td>
 				</tr>
 			</table> 
+	</center>		
 	</body>
 	</html> 
