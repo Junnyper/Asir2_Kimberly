75d38c77bb0d4318c132693734489648493c3435
Junnyper <kim_23_93@hotmail.com>
2019-11-11 16:17:07 +0100

    Ultimas cosas

--- asir2-timestamp.php
+++ asir2-timestamp.php
@@ -1,8 +1,11 @@
+Holiwi </br>
+<a href="https://192.168.0.36/asir2_James/asir2.html">Jaimis</a> </br>
+<a href="https://192.168.0.34/asir2_Kevin/asir2.html">Kev</a>
 <?php
 $dns=[
 	'192.168.0.105'=>'  Jaime',
 	'192.168.0.100'=>'  Jose Luis',
-	'192.168.0.99'=>'  Kim',
+	'::1'=>'  Kim',
 	];
 
 $f=fopen('all.txt','a');
